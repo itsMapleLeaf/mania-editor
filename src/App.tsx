e7298547732581334b271775d1283d6d5b0d369e
kingdaro <kingdaro@gmail.com>
2020-02-10 20:08:29 -0600

    open osu file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react"
+import { remote } from "electron"
+import React, { useState } from "react"
 
 function App() {
-  return <h1>hey look it's an app i think</h1>
+  const [content, setContent] = useState<string>()
+
+  const showOpenDialog = () => {
+    remote.dialog
+      .showOpenDialog({ properties: ["openFile"] })
+      .then((result) => {
+        const [filePath] = result.filePaths
+        return remote.require("fs").promises.readFile(filePath, "utf8")
+      })
+      .then(setContent)
+      .catch((error) => alert(error?.message || String(error)))
+  }
+
+  return (
+    <main>
+      {content ?? <button onClick={showOpenDialog}>load .osu file</button>}
+    </main>
+  )
 }
 
 export default App
