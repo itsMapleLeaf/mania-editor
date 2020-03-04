ef2e1db035ba98f54f013ffb103214ebd7f73df2
kingdaro <kingdaro@gmail.com>
2020-03-03 21:21:28 -0600

    App -> App.old

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { Global, ThemeProvider } from "@emotion/react"
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./app/App"
+import App from "./app/App.old"
 import { reset } from "./ui/reset"
 import { darkTheme } from "./ui/theme"
 
